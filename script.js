6b6e4d46bd534f68dff394bae9910defb8a20de1
Barna Keszler <44028159+bkeszl@users.noreply.github.com>
2020-12-16 19:10:57 +0100

    Bkeszl static (#2)
    * feat(viMuseoMobile): add html content
    
    * material(viMuseoMobile): fetch illustration
    
    * feat(viMuseoMobile): nav, cta
    
    * feat(ViMuseoMobile): hero image, online experiences
    
    * feat(viMuseoMobile): search, footer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
   var x = document.getElementsByClassName("nav-links");
-  if (x[0].style.display === "block") {
+  if (x[0].style.display === "grid") {
     x[0].style.display = "none";
   } else {
-    x[0].style.display = "block";
+    x[0].style.display = "grid";
   }
 }
